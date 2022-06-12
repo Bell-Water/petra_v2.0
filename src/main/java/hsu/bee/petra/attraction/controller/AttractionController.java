003048f9c98983b7e3e43e157b16048ee983c6d7
JSP <pjs12041204@google.com>
2022-06-12 21:37:36 +0900

    api 위도,경도,범위 예외처리

--- src/main/java/hsu/bee/petra/attraction/controller/AttractionController.java
+++ src/main/java/hsu/bee/petra/attraction/controller/AttractionController.java
@@ -32,12 +32,23 @@ public class AttractionController {
 
     @GetMapping("attraction/surroundings")
     public Response<ArrayList<AddressDto>> getSurroundingAttractions(
-            @RequestParam(value="x") Double x,
-            @RequestParam(value="y") Double y,
-            @RequestParam(value="distance", required=false, defaultValue= "0.5") Double distance) {
+            @RequestParam(value="x") String x,
+            @RequestParam(value="y") String y,
+            @RequestParam(value="distance", required=false, defaultValue= "0.5") String distance) {
 
 
-        List<AddressDto> surroundingAttractions = attractionService.getSurroundingAttractions(x, y, distance);
+        double lat = 0.0;
+        double lon = 0.0;
+        double dist = 0.5;
+        try{
+            lat = Double.parseDouble(x);
+            lon = Double.parseDouble(y);
+            dist = Double.parseDouble(distance);
+        } catch(NumberFormatException e) {
+
+            throw new IllegalArgumentException("위도, 경도, 범위 재입력 바람");
+        }
+        List<AddressDto> surroundingAttractions = attractionService.getSurroundingAttractions(lat, lon, dist);
 
         return new Response(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, surroundingAttractions);
     }
